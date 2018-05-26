24807c37ab8bdf5f2b11600caa3a70f101e23ff1
Bram Zerbe <hitmanbobina47@gmail.com>
2018-05-26 18:45:41 -0400

    testing jenkins auto-testing not going to be a real unit test

---
+++ tests/maths_tests.py
@@ -0,0 +1,14 @@
+def addition_test():
+    assert 4 + 4 == 8
+
+def subtraction_test():
+    assert 4 - 4 == 0
+
+def multiply_test():
+    assert 4 * 4 == 16
+
+def divide_test():
+    assert 4 / 4 == 1
+
+def power_test():
+    assert 4 ** 4 == 256
