17df7c25a8e59c1d47083c54d6da66cb7e2eb9e7
Bram Zerbe <hitmanbobina47@gmail.com>
2018-05-26 22:15:07 -0400

    Fixed the tests

--- tests/db_tests.py
+++ tests/db_tests.py
@@ -1,25 +1,26 @@
 from family_task_queue import db
 from flask import Flask
-import tempfile
+import tempfile, os
 
 class Test_DB():
     def __init__(self):
         app = None
 
     def setup(self):
-        app = Flask(__name__)
-        _, temp_db = tempfile.mkstemp()
-        app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:////{temp_db}"
-        app.config["TESTING"] = True
-        db.init_app(app)
-        db.db.drop_all()
-        db.db.create_all()
+        self.app = Flask(__name__)
+        db_path = os.path.join(tempfile.gettempdir(), "test.db")
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+        self.app.config["TESTING"] = True
+        db.init_app(self.app)
+        db.db.drop_all(app=self.app)
+        db.db.create_all(app=self.app)
 
     def teardown(self):
-        db.db.drop_all()
+        db.db.drop_all(app=self.app)
 
     def test_create_family(self):
-        assert len(db.user.User.query.all()) == 0
-        db.user.create_family("This Is A Test")
-        assert len(db.user.User.query.all()) == 1
-        assert db.user.User.query.first().name == "This Is A Test"
+        with self.app.app_context():
+            assert len(db.user.Family.query.all()) == 0
+            db.user.create_family("This Is A Test")
+            assert len(db.user.Family.query.all()) == 1
+            assert db.user.User.query.first().name == "This Is A Test"
