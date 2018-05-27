c289c5c44f90238463917b85b058b2aab830432a
Bram Zerbe <hitmanbobina47@gmail.com>
2018-05-27 10:09:38 -0400

    Added family_id and author_id columns to db.task.Task model

--- family_task_queue/db/task.py
+++ family_task_queue/db/task.py
@@ -3,5 +3,7 @@ from .db import db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    family = db.Column(db.String(80), nullable=False)
-    author = db.Column(db.String(80), nullable=False)
+    family = db.relationship("Family", backref=db.backref('tasks', lazy=True))
+    family_id = db.Column(db.Integer, db.ForeignKey("family.id"), nullable=False)
+    author = db.relationship("User", backref=db.backref('created_tasks', lazy=True))
+    author_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
