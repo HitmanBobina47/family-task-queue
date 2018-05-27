31f29667580fd266853b6bd1e613e609b50ec51d
Bram Zerbe <hitmanbobina47@gmail.com>
2018-05-26 21:17:36 -0400

    Added db.user - Holder for User and Family db models Added db.user import to db.__init__

--- family-task-queue/db/__init__.py
+++ family-task-queue/db/__init__.py
@@ -1,5 +1,6 @@
 import .db
 import .task
+import .user
 
 def init_app(app):
     db.db.init_app(app)
---
+++ family-task-queue/db/user.py
@@ -0,0 +1,15 @@
+import .db.db as db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    family_id = db.Column(db.Integer, db.ForeignKey("family.id"), nullable=False)
+    family = db.relationship("Family", backref=db.backref('members', lazy=True))
+    roles = db.Column(db.PickleType(), nullable=False)
+
+
+class Family(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
